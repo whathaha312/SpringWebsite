4ab05a5e8d9d6080fa0161af32a293743ddca738
whathaha <609760626@qq.com>
2016-07-07 13:22:05 +0800

    practice spring and first commit

---
+++ src/main/java/com/website/DemonewsApplication.java
@@ -0,0 +1,12 @@
+package com.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonewsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/website/DemonewsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.website;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemonewsApplication.class)
+@WebAppConfiguration
+public class DemonewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
